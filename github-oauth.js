84f840d6f0e496d227573ea5e9e92cfe91e3df7d
Jurgen Leschner <jldec@ciaosoft.com>
2015-07-16 10:53:10 -0400

    get user email for authz

--- github-oauth.js
+++ github-oauth.js
@@ -7,6 +7,7 @@
 var debug   = require('debug')('pub:github');
 var qs      = require('querystring');
 var ms      = require('ms');
+var u       = require('pub-util');
 
 module.exports = function githubOAuth(server) {
 
@@ -18,7 +19,10 @@ module.exports = function githubOAuth(server) {
   var opts = server.opts.github || {};
 
   var request = require('request')
-    .defaults({ timeout:('timeout' in opts ? ms(opts.timeout) : 4000) });
+    .defaults(
+      { timeout: ('timeout' in opts ? ms(opts.timeout) : 4000),
+        headers: { 'User-agent':'pub-gatekeeper' } }
+    );
 
   var url  = opts.url || process.env.GHU || '/server/auth/github';
 
@@ -43,7 +47,7 @@ module.exports = function githubOAuth(server) {
   // calls github api to turn temporary code into access token
   app.get(url + '/callback', function(req, res) {
     debug('code: ' + req.query.code);
-    authenticate(req.query.code, function(err, result) {
+    authenticate(req, function(err, result) {
       debug(err || result);
       if (req.session) {
         var gh = req.session.github;
@@ -62,16 +66,39 @@ module.exports = function githubOAuth(server) {
   });
 
   // get access token from github
-  function authenticate(code, cb) {
+  function authenticate(req, cb) {
     request.post(
       'https://github.com/login/oauth/access_token',
       { form:
         { client_id: process.env.GHID,      // don't store credentials in opts
           client_secret: process.env.GHCS,  // get them straight from process.env
-          code: code } },
+          code: req.query.code } },
       function(err, resp, body) {
         if (err) return cb(err);
-        cb(null, qs.parse(body));
+        var result = qs.parse(body);
+        getUser(req, result, function() {
+          cb(null, result);
+        });
+      }
+    );
+  }
+
+  // try to get email for authz/ACLs - doesn't overwrite session.user
+  function getUser(req, result, cb) {
+    if (!result || !result.access_token) return process.nextTick(cb);
+    request.get(
+      'https://api.github.com/user/emails?access_token=' + result.access_token,
+      { headers:{ 'Accept':'application/vnd.github.v3+json' } },
+      function(err, resp, body) {
+        if (err) { debug(err); return cb(err); }
+        var list = JSON.parse(body); debug(list);
+        var email = u.find(list, function(entry) { return entry.primary; });
+        if (email) {
+          email = email.email;
+          if (req.session && !req.session.user) { req.session.user = email; }
+          result.user = email;
+        }
+        cb();
     });
   }
 
