cbeead9108371037cee41e108f654d5cc03a0182
jldec <jurgen@ciaosoft.com>
2022-01-22 22:12:55 +0000

    v1.2.10 deps

--- github-oauth.js
+++ github-oauth.js
@@ -1,7 +1,7 @@
 /**
  * pub-pkg github-oauth.js
  *
- * copyright 2015-2020, Jürgen Leschner - github.com/jldec - MIT license
+ * Copyright (c) 2015-2022 Jürgen Leschner - github.com/jldec - MIT license
 **/
 
 var debug   = require('debug')('pub:github');
